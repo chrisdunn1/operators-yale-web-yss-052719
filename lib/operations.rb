3ab6c5eadda7c2bd95867d8780dd6513f2948a31
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-05-08 18:10:54 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,17 @@
 def unsafe?(speed)
 
-	puts speed < 40 || speed > 60 ? "unsafe" : "safe"
-
+	if speed < 40 || speed > 60
+			puts "unsafe"
+		else"safe"
 end
 
 
 
 def not_safe?(speed)
 
+	puts speed < 40 || speed > 60 ? "unsafe" : "safe"
+
 end
 
 unsafe?(20)
+not_safe?(50)
